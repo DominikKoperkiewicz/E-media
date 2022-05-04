b1b82e3282aaae4e913fd41d308d1f4fdb99c396
Kacper Raczynski <252963@student.pwr.edu.pl>
2022-05-04 18:12:04 +0200

    added 1st version of fft

---
+++ fourier.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.io import imread, imshow
+from skimage.color import rgb2hsv, rgb2gray, rgb2yuv
+from skimage import color, exposure, transform
+from skimage.exposure import equalize_hist
+
+dark_image = imread('Lenna.png')
+
+dark_image_grey = rgb2gray(dark_image)
+plt.figure(num=None, figsize=(8, 6), dpi=80)
+plt.imshow(dark_image_grey, cmap='gray')
+plt.show()
+
+dark_image_grey_fourier = np.fft.fftshift(np.fft.fft2(dark_image_grey))
+plt.figure(num=None, figsize=(8, 6), dpi=80)
+plt.imshow(np.log(abs(dark_image_grey_fourier)), cmap='gray')
+plt.show()
